ea2d87a011746f700e5d5c3019e76270a42f96cb
gottajsx <gottajsx@gmail.com>
2026-01-14 18:55:52 +0100

    Ending TabsLayout()

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -14,17 +14,46 @@ export default function TabsLayout() {
                     )
                 }}
             />
+
              <Tabs.Screen 
                 name='friends' 
+                options={{
+                    title: 'friends',
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="users" size={24} color={color} />
+                    )
+                }}
             />
+            
             <Tabs.Screen 
                 name='newPost' 
+                options={{
+                    title: 'New Post',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="plus-square" size={24} color={color} />
+                    )
+                }}
             />
+            
             <Tabs.Screen 
                 name='inbox' 
+                options={{
+                    title: 'Inbox',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="message-minus-outline" size={24} color={color} />
+                    )
+                }}
             />
+            
             <Tabs.Screen 
                 name='profile' 
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="user" size={24} color={color} />
+                    )
+                }}
             />
         </Tabs>
     )
