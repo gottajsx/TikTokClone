4d0d6be4c2e4b121d79109a439f042c59bfadcaf
gottajsx <gottajsx@gmail.com>
2026-01-17 15:35:03 +0100

    Add heart button on PostListItem page

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,40 +1,82 @@
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from "react-native"
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
 import { useVideoPlayer, VideoView } from 'expo-video';
+import { Ionicons } from '@expo/vector-icons';
 
 const videoSource =
   'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
 export default function PostListItem() {
-    const { height } = Dimensions.get('window');
-    const player = useVideoPlayer(videoSource, player => {
-        player.loop = true;
-        player.play();
-    });
+  const { height } = Dimensions.get('window');
+  const player = useVideoPlayer(videoSource, player => {
+  player.loop = true;
+  player.play();
+});
     
-    return (
-        <View style={{ height }}>
-            <VideoView 
-                style={{ flex: 1 }} 
-                player={player} 
-                contentFit="cover" 
-                nativeControls={false} />
-        </View>
-    )
-}
+  return (
+    <View style={{ height: height - 80 }}>
+      <VideoView 
+        style={{ flex: 1 }} 
+        player={player} 
+        contentFit="cover" 
+        nativeControls={false} 
+      />
+
+      <View style={styles.interactionBar}>
+        <TouchableOpacity style={styles.interactionButton} onPress={() => console.log('Like Pressed')}>
+          <Ionicons name="heart" size={33} color="#fff" />
+          <Text style={styles.interactionText}>{0}</Text>
+        </TouchableOpacity>
+      </View>
+
+        
+    </View>
+
+    
+  );
+};
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    padding: 10,
+  interactionBar: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
     alignItems: 'center',
+    gap: 25
+  },
+  interactionButton: {
+    alignItems: 'center',
+    gap: 5
+  },
+  interactionText: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: '600'
+  },
+  avatar: {
+    width: 35,
+    height: 35,
+    backgroundColor: '#fff',
+    borderRadius: 20,
     justifyContent: 'center',
-    paddingHorizontal: 50,
+    alignItems: 'center'
   },
-  video: {
-    width: 350,
-    height: 275,
+  avatarText: {
+    fontSize: 25,
+    fontWeight: 'bold'
   },
-  controlsContainer: {
-    padding: 10,
+  videoInfo: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 100,
+    gap: 5
   },
+  username: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  description: {
+    color: '#fff'
+  }
 });
